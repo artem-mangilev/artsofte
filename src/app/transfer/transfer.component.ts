91b4a15899de9ca7b69efe97e02ddb5c387e4931
Artem <artmnglv@gmail.com>
2020-12-05 19:00:21 +0500

    Store new transfer on submit

--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Transfer, TransfersStoreService } from '../transfers-store.service'
 
 @Component({
   selector: 'app-transfer',
@@ -9,17 +10,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class TransferComponent implements OnInit {
   form: FormGroup
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private transfersStoreService: TransfersStoreService
+  ) {}
 
   ngOnInit(): void {
     const validCardRegex = /^\d{4} \d{4} \d{4} \d{4}$/
 
     this.form = this.fb.group({
-      senderCardNumber: ['', [Validators.pattern(validCardRegex)]],
       senderName: ['', [Validators.required]],
+      senderCardNumber: ['', [Validators.pattern(validCardRegex)]],
       senderCardExpirationDate: ['', [Validators.required]],
       receiverCardNumber: ['', [Validators.pattern(validCardRegex)]],
       moneyAmount: [0, [Validators.required, Validators.min(1)]],
     })
+  }
+
+  onSubmit(formData) {
+    const transfer: Transfer = {
+      ...formData,
+      senderCardExpirationDate: new Date(formData.senderCardExpirationDate),
+      transferDate: new Date(),
+    }
+
+    this.transfersStoreService.addTransfer(transfer)
   }
 }
