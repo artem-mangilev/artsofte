3aac295f7d5a26e091f3518bfb32fe1e775cf517
Artem <artmnglv@gmail.com>
2020-12-05 13:25:06 +0500

    Create pages and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { HistoryComponent } from './history/history.component'
+import { TransferComponent } from './transfer/transfer.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'transfer',
+    component: TransferComponent,
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/transfer',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/transfer',
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
