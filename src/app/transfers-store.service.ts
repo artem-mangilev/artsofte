ea4f935fac00497d89f7036bd7cd9bf0a18b311e
Artem <artmnglv@gmail.com>
2020-12-06 13:33:48 +0500

    Store transfers in LocalStorage

--- src/app/transfers-store.service.ts
+++ src/app/transfers-store.service.ts
@@ -13,14 +13,18 @@ export interface Transfer {
   providedIn: 'root',
 })
 export class TransfersStoreService {
-  private transfers: Transfer[] = []
+  private transfers: Transfer[] = this.initTransfers()
 
   addTransfer(transfer: Transfer): void {
     this.transfers.push(transfer)
+
+    this.updateTransfersInStorage()
   }
 
   removeTransfer(transderId: number): void {
     this.transfers.splice(transderId, 1)
+
+    this.updateTransfersInStorage()
   }
 
   getAllTransfers(): Transfer[] {
@@ -29,5 +33,21 @@ export class TransfersStoreService {
 
   getTransfer(transferId: number): Transfer {
     return this.transfers[transferId]
+  }
+
+  private initTransfers(): Transfer[] {
+    const transfersStr = localStorage.getItem('transfers')
+    const transfers = JSON.parse(transfersStr, (key, value) => {
+      if (key === 'transferDate' || key === 'senderCardExpirationDate') {
+        return new Date(value)
+      }
+      return value
+    })
+
+    return transfers === null ? [] : transfers
+  }
+
+  private updateTransfersInStorage() {
+    localStorage.setItem('transfers', JSON.stringify(this.transfers))
   }
 }
